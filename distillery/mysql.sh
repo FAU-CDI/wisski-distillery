d68c0f3e75ef047d813ffd54c3ad845bdd8f2bac
Tom Wiesing <tkw01536@gmail.com>
2020-06-28 12:29:44 +0200

    Make scripts executable

--- distillery/mysql.sh
+++ distillery/mysql.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-# disable the log
-DISABLE_LOG=1
-
 # read the lib/shared.sh
+DISABLE_LOG=1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 source "$DIR/lib/lib.sh"
