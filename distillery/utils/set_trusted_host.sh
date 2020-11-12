e856afdc93e7007b53e57f96b78252955fcb3220
Tom Wiesing <tkw01536@gmail.com>
2020-11-12 18:57:31 +0100

    Rename composer hostnames
    Previously, all containers had hostnames corresponding to their domains.
    This made it impossible to use curl and friends from within the
    containers. This commit renames the hostnames of the containers to
    append a '.wisski' suffix.

--- distillery/utils/set_trusted_host.sh
+++ distillery/utils/set_trusted_host.sh
@@ -3,7 +3,7 @@
 # This utility script can be used to configure the trusted host settings inside of settings.php. 
 # It doesn't take care of corner cases and should only be used when needed. 
 
-INSTANCE_DOMAIN="$(hostname -f)"
+INSTANCE_DOMAIN="${VIRTUAL_HOST}"
 chmod u+w web/sites/default/settings.php
 echo "" >> web/sites/default/settings.php
 echo "\$settings['trusted_host_patterns'] = ['${INSTANCE_DOMAIN//\./\\\.}'];" >> web/sites/default/settings.php
