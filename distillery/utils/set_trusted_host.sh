ab05656bfe1b99601b1206b5cf785bff1a128df4
Tom Wiesing <tkw01536@gmail.com>
2020-11-12 19:01:56 +0100

    set_trusted_host: Fix hostname

--- distillery/utils/set_trusted_host.sh
+++ distillery/utils/set_trusted_host.sh
@@ -3,7 +3,8 @@
 # This utility script can be used to configure the trusted host settings inside of settings.php. 
 # It doesn't take care of corner cases and should only be used when needed. 
 
-INSTANCE_DOMAIN="${VIRTUAL_HOST}"
+INSTANCE_DOMAIN="$(hostname -f)"
+INSTANCE_DOMAIN="${INSTANCE_DOMAIN%.wisski}"
 chmod u+w web/sites/default/settings.php
 echo "" >> web/sites/default/settings.php
 echo "\$settings['trusted_host_patterns'] = ['${INSTANCE_DOMAIN//\./\\\.}'];" >> web/sites/default/settings.php
