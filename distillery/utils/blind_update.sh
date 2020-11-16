19da0361f785cf83785ec8a809a5217c3d718e00
Tom Wiesing <tkw01536@gmail.com>
2020-11-16 15:53:53 +0100

    blind_update: Update WissKI dependencies

--- distillery/utils/blind_update.sh
+++ distillery/utils/blind_update.sh
@@ -3,11 +3,14 @@
 # This utility script can be used to blindly update all dependencies to their latest versions. 
 # It does not perform any checking whatsoever. 
 
+# update the main modules
 cd /var/www/data/project || exit 1
-
-# composer install updates
 chmod u+rw web/sites/default/
 composer update
 
-# update the dabatabase
+# update the db
 drush -y updatedb
+
+# update the wisski dependencies
+cd /var/www/data/project/web/modules/contrib/wisski || exit 1
+composer update
