3e631b3a378ed8629c89aebb95e0e9f237408ef3
Tom Wiesing <tkw01536@gmail.com>
2023-03-16 11:35:47 +0100

    cron: wrong error message

--- internal/wisski/ingredient/barrel/drush/cron.go
+++ internal/wisski/ingredient/barrel/drush/cron.go
@@ -15,7 +15,7 @@ import (
 )
 
 var errCronFailed = exit.Error{
-	Message:  "failed to run cron script for instance %q: exited with code %s",
+	Message:  "failed to run cron script for instance %q: exited with code %d",
 	ExitCode: exit.ExitGeneric,
 }
 
