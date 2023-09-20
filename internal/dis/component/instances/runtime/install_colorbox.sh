181b474c8fa13246b4bf36ca35f2f1274c3f0ec4
Tom Wiesing <tkw01536@gmail.com>
2023-09-20 10:10:48 +0200

    runtime: Mount new runtime directory
    This commit updates the runtime directory to include a couple of utility
    scripts. These are mounted into every directory.

--- internal/dis/component/instances/runtime/install_colorbox.sh
+++ internal/dis/component/instances/runtime/install_colorbox.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 set -e
 
-# make a temporary directory and cd into it
-TEMPDIR=$(mktemp -d)
-pushd "$TEMPDIR"
-trap 'popd && rm -rf $TEMPDIR' EXIT
-
-# curl the colorbox zip and unpack it
-curl -L https://github.com/jackmoore/colorbox/archive/master.zip --output master.zip
-unzip master.zip
-
-# make the directory for libraries, and remove the old colorbox installation
-chmod u+rw /var/www/data/project/web/sites/default/
-mkdir -p /var/www/data/project/web/sites/default/libraries/
-rm -rf /var/www/data/project/web/sites/default/libraries/colorbox
-
-# copy over the new installation
-mv colorbox-master/ /var/www/data/project/web/sites/default/libraries/colorbox
-
+# make the target directory and install
+mkdir -p /var/www/data/project/web/sites/default/libraries/colorbox
+curl -L https://raw.githubusercontent.com/jackmoore/colorbox/master/LICENSE.md -o /var/www/data/project/web/sites/default/libraries/colorbox/LICENSE.md
+curl -L https://raw.githubusercontent.com/jackmoore/colorbox/master/jquery.colorbox-min.js -o /var/www/data/project/web/sites/default/libraries/colorbox/jquery.colorbox-min.js
