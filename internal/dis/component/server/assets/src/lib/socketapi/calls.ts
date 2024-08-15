3dcd1177187b03032cb7a36ae9429996782fcb34
Tom Wiesing <tkw01536@gmail.com>
2024-08-15 21:38:31 +0200

    Update version typings

--- internal/dis/component/server/assets/src/lib/socketapi/calls.ts
+++ internal/dis/component/server/assets/src/lib/socketapi/calls.ts
@@ -18,7 +18,7 @@ interface ProvisionParams {
 }
 
 interface SystemParams {
-  PHP: 'Default (8.1)' | '8.0' | '8.1' | '8.2'
+  PHP: 'Default (8.1)' | '8.0' | '8.1' | '8.2' | '8.3'
   OpCacheDevelopment: boolean
   ContentSecurityPolicy: string
 }
