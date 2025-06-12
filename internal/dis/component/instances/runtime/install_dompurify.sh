98284c7fa56ec201ba8044548d2e9ab52064bfd5
Tom Wiesing <tkw01536@gmail.com>
2025-06-12 15:45:05 +0200

    install_* scripts: Use Drush instead

--- internal/dis/component/instances/runtime/install_dompurify.sh
+++ internal/dis/component/instances/runtime/install_dompurify.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -e
 
-echo "=> Creating 'sites/default/libraries/DOMPurify/dist/' directory"
-mkdir -p /var/www/data/project/web/libraries/DOMPurify/dist/
-
-echo "=> Downloading 'purify.min.js' and 'LICENSE'"
-curl -L https://raw.githubusercontent.com/cure53/DOMPurify/main/dist/purify.min.js -o /var/www/data/project/web/libraries/DOMPurify/dist/purify.min.js
-curl -L https://raw.githubusercontent.com/cure53/DOMPurify/main/LICENSE -o /var/www/data/project/web/libraries/DOMPurify/LICENSE
-
-echo "=> Done"
+drush colorbox:dompurify
